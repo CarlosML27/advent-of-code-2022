9ca855c9c7fa75c3dad03758910d4579bb079bf9
Carlos Morente <carlosmorentedev@gmail.com>
2022-12-06 16:44:43 +0100

    feat(solutions): Solve day 02 puzzle 2
    Add solution to solve day 02 puzzle 2.

--- __main__.py
+++ __main__.py
@@ -1,4 +1,4 @@
-from aoc22 import run_01_1, run_01_2, run_02_1
+from aoc22 import run_01_1, run_01_2, run_02_1, run_02_2
 
 if __name__ == '__main__':
     print('-------')
@@ -10,4 +10,7 @@ if __name__ == '__main__':
     print('-------')
     print('Day 02, Puzzle 1:')
     run_02_1()
+    print('-------')
+    print('Day 02, Puzzle 2:')
+    run_02_2()
     print('-------')
--- aoc22/solutions/day_02/puzzle_2.py
+++ aoc22/solutions/day_02/puzzle_2.py
@@ -2,11 +2,23 @@ from aoc22.inputs import get_relative_path, read_lines
 from aoc22.solutions.day_02.puzzle_commons import get_outcome_result, get_strategy_guide
 
 
+def _get_player_result(rival_score: int, instruction: int) -> int:
+    player_score = rival_score
+    if instruction == 1:
+        rival_score = rival_score + 3 if rival_score == 1 else rival_score
+        player_score = rival_score - 1
+    elif instruction == 3:
+        rival_score = 0 if rival_score == 3 else rival_score
+        player_score = rival_score + 1
+    return player_score
+
+
 def _get_round_result(game_round: tuple[str, str]) -> int:
     ascii_uppercase_a_value = 65
     ascii_uppercase_x_value = 88
     rival_score = ord(game_round[0]) - ascii_uppercase_a_value + 1
-    player_score = ord(game_round[1]) - ascii_uppercase_x_value + 1
+    instruction = ord(game_round[1]) - ascii_uppercase_x_value + 1
+    player_score = _get_player_result(rival_score, instruction)
     outcome = get_outcome_result(player_score, rival_score)
     return player_score + outcome
 
