169f71f8fd682d947317eda925fdf1530efaee31
Carlos Morente <carlosmorentedev@gmail.com>
2022-12-06 12:53:05 +0100

    refactor(solutions): Separate puzzles common logic
    Separate common logic between puzzles of the same day, using a commons
    file containing common logic.

--- aoc22/solutions/day_01/puzzle_1.py
+++ aoc22/solutions/day_01/puzzle_1.py
@@ -1,19 +1,5 @@
 from aoc22.inputs import get_relative_path, read_lines
-
-
-def _get_elves_values(lines: list[str]) -> list[int]:
-    size = len(lines)
-    index_list = [index + 1 for index, value in enumerate(lines) if value == '']
-    elves_lists = [
-        lines[start : end - 1]
-        for start, end in zip(
-            [0] + index_list,
-            index_list + ([size + 1] if index_list[-1] != size else []),
-        )
-        if start < end
-    ]
-    elves = [sum(map(int, elf_list)) for elf_list in elves_lists]
-    return elves
+from aoc22.solutions.day_01 import get_elves_values
 
 
 def _get_result(elves: list[int]) -> int:
@@ -23,6 +9,6 @@ def _get_result(elves: list[int]) -> int:
 def run():
     input_path = get_relative_path(__file__, 'input.txt')
     lines = read_lines(input_path)
-    elves = _get_elves_values(lines)
+    elves = get_elves_values(lines)
     result = _get_result(elves)
     print(result)
